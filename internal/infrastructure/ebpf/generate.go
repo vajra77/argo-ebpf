70d3294edee13da9946b2aafc3dd6816a5c1679b
fferreri <f.ferreri@namex.it>
2026-08-01 21:45:20 +0200

    refactored bpf interface

--- internal/infrastructure/ebpf/generate.go
+++ internal/infrastructure/ebpf/generate.go
@@ -1,3 +1,3 @@
 package ebpf
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc $CLANG -strip $LLVM_STRIP -target bpfel Bpf analyzer.c -- -I./includes
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc $CLANG -strip $LLVM_STRIP -target bpfel Bpf analyzer.c  -- -I./includes
