96e7c26e713f36ff65e5ab90b92d819afa534924
vajra77 <f.ferreri@namex.it>
2026-08-02 17:20:08 +0200

    fixing compilation path issues

--- internal/infrastructure/ebpf/generate.go
+++ internal/infrastructure/ebpf/generate.go
@@ -9,4 +9,4 @@
 
 package ebpf
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc $CLANG -strip $LLVM_STRIP -target bpfel Bpf analyzer.c  -- -I./includes
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc $CLANG -strip $LLVM_STRIP -target bpfel Bpf analyzer.bpf  -- -I./include
